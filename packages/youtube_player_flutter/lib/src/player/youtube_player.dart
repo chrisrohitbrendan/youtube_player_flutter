b2bdd0d6ea4a15c3c5964af05ea362590abfea02
Chris Rohit Brendan <rohit.brendan@gmail.com>
2023-11-12 11:34:55 +0000

    fix added

--- packages/youtube_player_flutter/lib/src/player/youtube_player.dart
+++ packages/youtube_player_flutter/lib/src/player/youtube_player.dart
@@ -395,7 +395,9 @@ class _YoutubePlayerState extends State<YoutubePlayer> {
             ),
           ],
           if (!controller.flags.hideControls)
-            const Center(child: PlayPauseButton()),
+            Center(
+              child: PlayPauseButton(bufferIndicator: widget.bufferIndicator),
+            ),
           if (controller.value.hasError) errorWidget,
         ],
       ),

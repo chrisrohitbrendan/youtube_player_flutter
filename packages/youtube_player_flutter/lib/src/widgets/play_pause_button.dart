34f6592d25a488b67ba4025ac709841cf5b90cfe
Chris Rohit Brendan <rohit.brendan@gmail.com>
2023-12-02 10:35:37 +0400

    Revert "ig ux for video"
    This reverts commit ec36e97df717ebfd3ec39a715a0d490b8d23b97a.

--- packages/youtube_player_flutter/lib/src/widgets/play_pause_button.dart
+++ packages/youtube_player_flutter/lib/src/widgets/play_pause_button.dart
@@ -30,7 +30,6 @@ class _PlayPauseButtonState extends State<PlayPauseButton>
     with TickerProviderStateMixin {
   late YoutubePlayerController _controller;
   late AnimationController _animController;
-  bool _mute = false;
 
   @override
   void initState() {
@@ -80,16 +79,6 @@ class _PlayPauseButtonState extends State<PlayPauseButton>
     }
   }
 
-  void unMute() {
-    _mute = false;
-    _controller.unMute();
-  }
-
-  void mute() {
-    _mute = true;
-    _controller.mute();
-  }
-
   @override
   Widget build(BuildContext context) {
     final value = _controller.value;
@@ -106,11 +95,9 @@ class _PlayPauseButtonState extends State<PlayPauseButton>
           color: Colors.transparent,
           child: InkWell(
             borderRadius: BorderRadius.circular(50.0),
-            onLongPress: () => _controller.pause(),
-            onTapUp: (details) {
-              if (!_controller.value.isPlaying) _controller.play();
-            },
-            onTap: () => _mute ? unMute() : mute(),
+            onTap: () => _controller.value.isPlaying
+                ? _controller.pause()
+                : _controller.play(),
             child: AnimatedIcon(
               icon: AnimatedIcons.play_pause,
               progress: _animController.view,
